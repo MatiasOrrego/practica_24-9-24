54da69b61347b308027c174ef34155a0f01ecec1
MatiasOrrego <orregomatias50@gmail.com>
2024-09-24 21:24:21 -0300

    los usuarios pueden ver sus tareas creadas

--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { getAllTodosCtrl } from "../controllers/todos.controllers.js";
+import validarJWT from "../middlewares/validar-jwt.js";
 
 const todosRouter = Router();
 
-todosRouter.get("/", getAllTodosCtrl);
+todosRouter.get("/",validarJWT , getAllTodosCtrl);
 
 export { todosRouter };
