d21ab295396ff9fa8a1963b7a5b5c9a9dc7feb69
OrregoMatias <orregomatias50@gmail.com>
2024-09-25 15:22:35 -0300

    actualice las rutas

--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getAllTodosCtrl } from "../controllers/todos.controllers.js";
+import { getAllTodosCtrl, createTodoCtrl, updateTodoCtrl, deleteTodoCtrl } from "../controllers/todos.controllers.js";
 import validarJWT from "../middlewares/validar-jwt.js";
 
 const todosRouter = Router();
 
 todosRouter.get("/",validarJWT , getAllTodosCtrl);
+todosRouter.post("/",validarJWT , createTodoCtrl);
+todosRouter.put("/:id",validarJWT , updateTodoCtrl);
+todosRouter.delete("/:id",validarJWT , deleteTodoCtrl);
 
 export { todosRouter };
